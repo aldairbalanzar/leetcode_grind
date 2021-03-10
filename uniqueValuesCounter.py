ba0de7a677ae80e951de3084e757f5fe81e5e4d6
aldairbalanzar <abalanzar7@gmail.com>
2021-03-10 09:56:51 -0500

    uniqueValuesCounter - done

---
+++ uniqueValuesCounter.py
@@ -0,0 +1,19 @@
+# Function that finds the amount unique values in array.open
+def uniqueValuesCounter(arr):
+  if len(arr) < 1:
+    print('This array is empty.')
+
+  pointer1 = 0
+  pointer2 = 1
+  counter = 1
+  while pointer2 <= len(arr) - 1:
+    if arr[pointer1] != arr[pointer2]:
+      print('unique value')
+      counter += 1
+      pointer1 += 1
+      pointer2 += 1
+    else:
+      pointer1 += 1
+      pointer2 += 1
+  
+  return counter
