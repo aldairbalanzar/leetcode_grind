37df87ceba52f30206290cca9f78314da3eea1c1
aldairbalanzar <abalanzar7@gmail.com>
2021-03-11 01:23:23 -0500

    maxSubArray - done

---
+++ maxSubArraySum.py
@@ -0,0 +1,19 @@
+# finds max sum of subarray that is determined by given size
+def maxSubArraySum(arr, num):
+  if num > len(arr):
+    print('array is too small for chosen sub array size.')
+    return False
+
+  else:
+    maxSum = 0
+    currSum = 0
+    for i in range(num):
+      maxSum += arr[i]
+    
+    currSum = maxSum
+    for i in range(num, len(arr)):
+      currSum = currSum - arr[i - num] + arr[i]
+      if currSum > maxSum:
+        maxSum = currSum
+
+    return maxSum
